cc368a83e2d7469870d351345b65f3196510a39c
dragan.mladjenovic <dragan.mladjenovic@rt-rk.com>
2019-02-07 20:19:05 +0100

    GTM19-172: nanoMIPS: Bad codegen for lazy-bound calls with -mcmodel=auto
    gcc/
        * gcc/cfgcleanup.c(old_insns_match_p): Check for REG_CALL_DECL note.
    
    gcc/testsuite/
                  * gcc.target/gcc.target/nanomips/cfgcleanup-jalr.c: New test.
    
    (cherry picked from commit 92f3d978f39af5777f594885eef0b0ae885eada5)

--- gcc/cfgcleanup.c
+++ gcc/cfgcleanup.c
@@ -1230,7 +1230,13 @@ old_insns_match_p (int mode ATTRIBUTE_UNUSED, rtx_insn *i1, rtx_insn *i2)
 	    }
 	}
 
-      if (insn_callee_abi (i1) != insn_callee_abi (i2))
+      n1 = find_reg_note (i1, REG_CALL_DECL, 0);
+      n2 = find_reg_note (i2, REG_CALL_DECL, 0);
+
+      if (!n1 && n2)
+        return dir_none;
+
+      if (n1 && (!n2 || !rtx_equal_p (XEXP (n1, 0), XEXP (n2, 0))))
         return dir_none;
     }
 
