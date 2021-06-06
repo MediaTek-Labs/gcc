ecc3162f7adc16a34c773ce3ec65c9171e4b8d57
Dragan Mladjenovic <OT_Dragan.Mladjenovic@mediatek.com>
2021-06-06 21:59:10 +0200

    Remove test requiring -mhard-float
    gcc/testsuite/
    
            * gcc.target/nanomips/call-saved-1.c:
    	Remove mentions of FP registers.
            * gcc.target/nanomips/fpr-moves-3.c: Removed.
            * gcc.target/nanomips/get-fcsr-1.c: Removed.
            * gcc.target/nanomips/ldc1-1.c: Removed.
            * gcc.target/nanomips/ldc1-2.c: Removed.
            * gcc.target/nanomips/lwc1-1.c: Removed.
            * gcc.target/nanomips/lwc1-2.c: Removed.
            * gcc.target/nanomips/movdf-1.c: Removed.
            * gcc.target/nanomips/mxhc1.c: Removed.
            * gcc.target/nanomips/pr45074.c: Removed.
            * gcc.target/nanomips/rsqrt-3.c: Removed.
            * gcc.target/nanomips/sdc1-1.c: Removed.
            * gcc.target/nanomips/sdc1-2.c: Removed.
            * gcc.target/nanomips/set-fcsr-1.c: Removed.
            * gcc.target/nanomips/ssdata-const-hf.c: Removed.
            * gcc.target/nanomips/swc1-1.c: Removed.
            * gcc.target/nanomips/swc1-2.c: Removed.

--- gcc/testsuite/gcc.target/nanomips/call-saved-1.c
+++ gcc/testsuite/gcc.target/nanomips/call-saved-1.c
@@ -1,5 +1,5 @@
 /* Check that we save the correct call-saved GPRs and FPRs.  */
-/* { dg-options "-march=32r6 -m32 -mhard-float" } */
+/* { dg-options "-march=32r6 -m32" } */
 
 void bar (void);
 
@@ -11,20 +11,4 @@ foo (int x)
 }
 /* { dg-final { scan-assembler "\\\$s0-\\\$s7" } } */
 /* { dg-final { scan-assembler "\\\$(30|fp)" } } */
-/* { dg-final { scan-assembler "\\\$f8" } } */
-/* { dg-final { scan-assembler "\\\$f9" } } */
-/* { dg-final { scan-assembler "\\\$f10" } } */
-/* { dg-final { scan-assembler "\\\$f11" } } */
-/* { dg-final { scan-assembler "\\\$f12" } } */
-/* { dg-final { scan-assembler "\\\$f13" } } */
-/* { dg-final { scan-assembler "\\\$f14" } } */
-/* { dg-final { scan-assembler "\\\$f15" } } */
-/* { dg-final { scan-assembler-not "\\\$f0" } } */
-/* { dg-final { scan-assembler-not "\\\$f2" } } */
-/* { dg-final { scan-assembler-not "\\\$f6" } } */
-/* { dg-final { scan-assembler-not "\\\$f17" } } */
-/* { dg-final { scan-assembler-not "\\\$f23" } } */
-/* { dg-final { scan-assembler-not "\\\$f25" } } */
-/* { dg-final { scan-assembler-not "\\\$f27" } } */
-/* { dg-final { scan-assembler-not "\\\$f29" } } */
-/* { dg-final { scan-assembler-not "\\\$f31" } } */
+
--- gcc/testsuite/gcc.target/nanomips/pr45074.c
+++
@@ -1,8 +0,0 @@
-/* { dg-options "-mhard-float -mgp32 -ffixed-f20" } */
-register double g __asm__("$f20");
-
-void
-test (double a)
-{
-  g = -a;
-}
