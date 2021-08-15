MediaTek-Labs/gcc

commit 70a6fed80916a218b7b2561ce8b0a9f93bc93eea

mips.c (mips_print_operand): Remove 'y' operand code.
	* config/mips/mips.c (mips_print_operand): Remove 'y' operand code.
	* config/mips/mips.md (<GPR:d>lsa): Rewrite with shift operator.
	* config/mips/predicates.md (const_immlsa_operand): Remove log call.

From-SVN: r223101

gcc/config/mips/mips.c
@@ -8440,7 +8440,6 @@ mips_print_operand_punct_valid_p (unsigned char code)
    'x'	Print the low 16 bits of CONST_INT OP in hexadecimal format.
    'd'	Print CONST_INT OP in decimal.
    'm'	Print one less than CONST_INT OP in decimal.
-   'y'	Print exact log2 of CONST_INT OP in decimal.
    'h'	Print the high-part relocation associated with OP, after stripping
 	  any outermost HIGH.
    'R'	Print the low-part relocation associated with OP.
@@ -8500,19 +8499,6 @@ mips_print_operand (FILE *file, rtx op, int letter)
     case 'm':
       if (CONST_INT_P (op))
 	fprintf (file, HOST_WIDE_INT_PRINT_DEC, INTVAL (op) - 1);
-      else
-	output_operand_lossage ("invalid use of '%%%c'", letter);
-      break;
-
-    case 'y':
-      if (CONST_INT_P (op))
-	{
-	  int val = exact_log2 (INTVAL (op));
-	  if (val != -1)
-	    fprintf (file, "%d", val);
-	  else
-	    output_operand_lossage ("invalid use of '%%%c'", letter);
-	}
       else
 	output_operand_lossage ("invalid use of '%%%c'", letter);
       break;

commit 11b155ee46699bebf23b35db9440ff9d986d062e

Fix conditions for creating indexed scaled loads/stores
Fix costing for lsa broken by 70a6fed80916a218b7b2561ce8b0a9f93bc93eea
and tune it to make it more likely for fwprop to create indexed scaled
mem. This happens to work by accident for SI mode. Now it should
consistently fire for HI mode also.

gcc/

        * config/mips/mips.c (mips_rtx_costs):
	Open code const_immlsa_operand check using exact_log2.

gcc/testsuite/

        * gcc.target/nanomips/sx-1.c: Scan only for shxs pattern.
        * gcc.target/nanomips/sx-2.c: Ditto.

gcc/config/mips/mips.c
@@ -6105,11 +6105,19 @@ mips_rtx_costs (rtx x, machine_mode mode, int outer_code,
 	  && GET_CODE (XEXP (x, 0)) == MULT)
 	{
 	  rtx op2 = XEXP (XEXP (x, 0), 1);
-	  if (const_immlsa_operand (op2, mode))
+	  HOST_WIDE_INT op2_shift;
+
+	  if (CONST_INT_P (op2)
+	      && (op2_shift = exact_log2 (INTVAL (op2))) >= 0
+	      && IN_RANGE ((op2_shift - (TARGET_NANOMIPS ? 0 : 1)), 0, 3))
 	    {
+	      /* Icrease the cost slightly over the plain addition to allow
+		 creation of more indexed scaled addresess in fwprop */
 	      *total = (COSTS_N_INSNS (1)
+			+ ((TARGET_NANOMIPS == NANOMIPS_NMF) ? 1 : 0)
 			+ set_src_cost (XEXP (XEXP (x, 0), 0), mode, speed)
 			+ set_src_cost (XEXP (x, 1), mode, speed));
+
 	      return true;
 	    }
 	}

gcc/testsuite/gcc.target/nanomips/sx-1.c
@@ -11,10 +11,9 @@
 TEST (1, char, char)
 TEST (2, char, short)
 /* { dg-final { scan-assembler-times "\tsbx\t" 4 } } */
-/* { dg-final { scan-assembler-times "\tshxs\t" 4 } } */
 TEST (3, short, short)
+/* { dg-final { scan-assembler-times "\tshxs\t" 8 } } */
 TEST (4, short, int)
-/* { dg-final { scan-assembler-times "\tshx\t" 4 } } */
 /* { dg-final { scan-assembler-times "\tswxs\t" 4 } } */
 TEST (5, int, long long)
 /* { dg-final { scan-assembler-times "\tsw\t" 4 } } */

gcc/testsuite/gcc.target/nanomips/sx-2.c
@@ -9,6 +9,6 @@
 TEST (1, char)
 /* { dg-final { scan-assembler-times "\tsbx\t" 2 } } */
 TEST (2, short)
-/* { dg-final { scan-assembler-times "\tshx\t" 2 } } */
+/* { dg-final { scan-assembler-times "\tshxs\t" 2 } } */
 TEST (3, int)
 /* { dg-final { scan-assembler-times "\tswxs\t" 2 } } */