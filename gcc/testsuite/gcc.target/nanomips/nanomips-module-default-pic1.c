248bab1b09914e588567955ec8454cdb3e68185f
Dragan Mladjenovic <OT_Dragan.Mladjenovic@mediatek.com>
2022-01-17 06:41:00 +0800

    [BZ #21] nanomips-module-default-pic1.c fails for -fPIC
    gcc/testsuite/
    
    	* gcc.target/nanomips/nanomips-module-default-pic1.c:
    	Skip if -fPIC passed.

--- gcc/testsuite/gcc.target/nanomips/nanomips-module-default-pic1.c
+++ gcc/testsuite/gcc.target/nanomips/nanomips-module-default-pic1.c
@@ -1,6 +1,7 @@
 /* Default for linux is pic1  */
 /* { dg-do compile { target nanomips*-linux-* } }  */
 /* { dg-options "-march=32r6 -ffat-lto-objects" }  */
+/* { dg-skip-if "" { *-*-* } { "-fPIC" } { "" } }  */
 /* { dg-final { scan-assembler "\\\.module\tpic1" } }  */
 
 void foo (void) { }
